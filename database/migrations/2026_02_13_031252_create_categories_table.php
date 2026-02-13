dc8eef1fb7ac5f98658ea072dc573ed9d6fcfa90
santxjoya <109623621+santxjoya@users.noreply.github.com>
2026-02-12 22:20:49 -0500

    chore: DataBase Migration in Docker

--- database/migrations/2026_02_13_031252_create_categories_table.php
+++ database/migrations/2026_02_13_031252_create_categories_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-    }
+    public function up(): void
+{
+    Schema::create('categories', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('slug')->unique();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('categories');
     }
 };
